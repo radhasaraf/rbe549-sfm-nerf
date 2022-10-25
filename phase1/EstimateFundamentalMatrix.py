a89e9dd125de33cc55324b62bcd9cdd866367b86
Ramana Kiran <saikrn112@gmail.com>
2022-10-25 12:48:05 -0400

    removed stashed changes

--- phase1/EstimateFundamentalMatrix.py
+++ phase1/EstimateFundamentalMatrix.py
@@ -2,16 +2,18 @@
 def estimate_fundamental_matrix(x1, x2):
     """
     input: 
-        x1, x2 - non homogenous image feature coordinates
-                 N x 2
+        v1 - N x 2
+        v2 - N x 2
     output:
         F - fundamental matrix 3 x 3
     """
     # construct Ax = 0
     # get SVD of A
-    # get right most singular value # lookup homework1
+    # get right signular vector # lookup homework1
+    # reconstruct F from singular vector
     # take SVD of F
     # plot the epipolar lines with this F
+    #   expected output: all epipolar do not intersect at one point (or no epipole)
     # set 3rd element of D to 0
     # reestimate F with new D
     # make sure that rank of reestimated F is 2
