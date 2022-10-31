fix: Correct Rotation

phase1/ExtractCameraPose.py
@@ -39,9 +39,11 @@ def extract_camera_pose(E):
     Cs_final = []
     Rs_final = []
     eps = 1e-2
-    for C,R in zip(Cs,Rs):
+
+    for C, R in zip(Cs, Rs):
         det_value = np.linalg.det(R)
-        if -1 - eps < det_value and det_value < -1 + eps :
+
+        if -1 - eps < det_value < -1 + eps:
             Cs_final.append(-C)
             Rs_final.append(-R)
         else:

phase1/LinearTriangulation.py
@@ -27,7 +27,7 @@ def triangulate_points(K, C1, R1, C2, R2, v1, v2):
     P1 = K @ np.hstack((R1, T1)) # (3 x 3) @ (3 x 4) = 3 x 4
 
     C2 = C2.reshape((3,1))
-    T2 = - R2.T @ C2 # 3 x 1
+    T2 = - R2 @ C2 # 3 x 1
     P2 = K @ np.hstack((R2, T2)) # (3 x 3) @ (3 x 4) = 3 x 4
 
     # Construct sys of lin. equations
@@ -48,6 +48,3 @@ def triangulate_points(K, C1, R1, C2, R2, v1, v2):
 
     Xs = np.vstack(Xs) # N x 3
     return Xs
-
-    # sanity check: All Zs should be positive
-

phase1/Wrapper.py
@@ -31,7 +31,7 @@ def main(args):
     # get inliers RANSAC for all images
     corrected_pair_feat_matches = {}
     for key, value in img_pair_feat_matches.items():
-        corrected_pair_feat_matches[key] = get_inliers_RANSAC(value[0],value[1],1000,0.02)
+        corrected_pair_feat_matches[key] = get_inliers_RANSAC(value[0],value[1],1000,0.006)
 
     if args.debug:
         show_before_after_RANSAC(imgs, img_pair_feat_matches, corrected_pair_feat_matches)
@@ -58,8 +58,8 @@ def main(args):
     for C,R,color in zip(Cs,Rs,colors):
         Xs = triangulate_points(K, np.zeros(3), np.eye(3), C, R, v1, v2)
         Xs_all_poses.append(Xs)
-        #plt.scatter(Xs[:,0],Xs[:,2],color=color,marker='x')
-    #plt.show()
+    #     plt.scatter(Xs[:,0],Xs[:,2],color=color,marker='.')
+    # plt.show()
 
     # disambiguate the poses using chierality condition
     C, R, X = disambiguate_camera_poses(Cs, Rs, Xs_all_poses)